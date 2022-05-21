aafb920702bcf70eb6c82abebbe75c46c584819d
Piotr Zaborowski <pzabor007@gmail.com>
2022-05-21 13:45:25 +0200

    Added DB schema

--- backend/mysite/cvtbackend/models.py
+++ backend/mysite/cvtbackend/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=500)
+    content = models.CharField(max_length=10000)
+    views = models.BigIntegerField()
