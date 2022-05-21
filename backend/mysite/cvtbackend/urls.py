608ba0023ea2d141f421b35a9879e26eb01a7616
Piotr Zaborowski <pzabor007@gmail.com>
2022-05-21 15:42:51 +0200

    Created endpoint getting an article by id

--- backend/mysite/cvtbackend/urls.py
+++ backend/mysite/cvtbackend/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('article/<int:article_id>', views.article_by_id, name='article_by_id')
 ]
--- backend/mysite/cvtbackend/views.py
+++ backend/mysite/cvtbackend/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article
 
 
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def article_by_id(request, article_id):
+    article = Article.objects.get(pk=article_id)
+    print(len(Article))
+    return HttpResponse(f"{article.title};;;{article.content};;;{article.views}")
