e9e9fcad904ed9457f5b4ed984f133f0f3f1395b
Piotr Zaborowski <pzabor007@gmail.com>
2022-05-22 15:36:56 +0200

    Added containers for posts

--- frontend/cvt-frontend/src/App.tsx
+++ frontend/cvt-frontend/src/App.tsx
@@ -1,9 +1,29 @@
-import {Button, Container} from "@mui/material";
+import {Box, Button, Container, Grid, Stack} from "@mui/material";
 import Navbar from "./components/Navbar";
+import Feed from "./components/Feed";
 
 function App() {
   return (
-      <Navbar/>
+      <Box>
+        <Navbar/>
+          <Grid alignItems={"center"} justifyContent={"center"} marginLeft={'15%'} marginRight={'15%'}>
+              <Stack  spacing={2} margin={2}>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+                  <Feed/>
+              </Stack>
+          </Grid>
+
+      </Box>
   );
 }
 
--- frontend/cvt-frontend/src/components/Feed.tsx
+++ frontend/cvt-frontend/src/components/Feed.tsx
@@ -1,10 +1,28 @@
-import React from "react";
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
-
-const Feed = () => {
-    return(
-        <div>"TEST</div>
-    )
+export default function MultiActionAreaCard() {
+    return (
+        <Card sx={{ margin: 0 }}>
+            <CardActionArea>
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+                </Button>
+            </CardActions>
+        </Card>
+    );
 }
-
-export default Feed
