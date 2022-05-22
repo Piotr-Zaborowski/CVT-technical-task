7b6172867694fdbbe100da20fc046be2adc07511
Piotr Zaborowski <pzabor007@gmail.com>
2022-05-22 16:47:22 +0200

    Added delete, edit buttons

--- frontend/cvt-frontend/src/components/Feed.tsx
+++ frontend/cvt-frontend/src/components/Feed.tsx
@@ -3,10 +3,12 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import HomeIcon from "@mui/icons-material/Home";
+import {DeleteForever, Edit} from "@mui/icons-material";
 
 export default function MultiActionAreaCard() {
     return (
-        <Card sx={{ margin: 0 }}>
+        <Card variant="outlined" sx={{ margin: 0 }}>
             <CardActionArea>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
@@ -19,8 +21,15 @@ export default function MultiActionAreaCard() {
                 </CardContent>
             </CardActionArea>
             <CardActions>
-                <Button size="small" color="primary">
-                    Share
+                <Button style={{
+                    color: "#ff0000",
+                }} size="large" startIcon={<DeleteForever />}>
+                    DELETE
+                </Button>
+                <Button style={{
+                    color: "#68ee06",
+                }} size="large" startIcon={<Edit />}>
+                    EDIT
                 </Button>
             </CardActions>
         </Card>
